a3d9af28ba1637d94ab9906e96c36d32be4aaafb
Keith Thompson <keithm_thompson@outlook.com>
2016-10-26 16:50:44 -0400

    begin working on serving all assets

--- lib/server_connection.rb
+++ lib/server_connection.rb
@@ -1,20 +1,27 @@
 require 'rack'
 require './config/routes'
+require_relative './static_assets'
+require_relative './show_exceptions'
+require_relative  './db_connection'
 
 module Bezel
   class ServerConnection
     def self.start
-      app = proc do |env|
+      DBConnection.open
+
+      asset_app = Proc.new do |env|
         req = Rack::Request.new(env)
         res = Rack::Response.new
         ROUTER.run(req, res)
         res.finish
       end
 
-      Rack::Server.start(
-        app: app,
-        Port: 3000
-      )
+      app = Rack::Builder.new do
+        use Bezel::ShowExceptions
+        use Bezel::StaticAssets
+        run asset_app
+      end
+      run app
     end
   end
 end
